aeec1a35045615d5ddca80ffb9523c1da2898a9f
rmohanmd <rmohan.md@gmail.com>
2022-09-22 15:05:49 -0400

    Replaced extraneous div in renderedItems with React.Fragment

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,7 +3,7 @@ import React from "react";
 const Accordion = ({ items }) => {
   const renderedItems = items.map((item) => {
     return (
-      <div key={item.title}>
+      <React.Fragment key={item.title}>
         <div className="title active">
           <i className="dropdown icon"></i>
           {item.title}
@@ -11,11 +11,11 @@ const Accordion = ({ items }) => {
         <div className="content active">
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
 
-  return <div className="ui style accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
