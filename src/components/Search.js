d9d49a27d00c7c67371fa91c56074f3848df644d
rmohanmd <rmohan.md@gmail.com>
2022-09-22 17:00:31 -0400

    Parsed api request into a new array via useState

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("React");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
           origin: "*",
-          formart: "json",
+          format: "json",
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
 
     search();
