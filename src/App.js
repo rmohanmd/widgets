5e34a68410023736b5bc6935495e4846227666e8
rmohanmd <rmohan.md@gmail.com>
2022-09-22 14:55:21 -0400

    Created items array and pass as prop to Accordion component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [
+  {
+    title: "What exactly is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content:
+      "React is a very popular and widely used JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "you use react by creating components",
+  },
+];
 export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
