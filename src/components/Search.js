fe4dcbb4041b3d418751ad050540cd2cec9f052a
rmohanmd <rmohan.md@gmail.com>
2022-09-22 17:36:38 -0400

    Added dangerouslySet... to deal with html in json package provided by wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,15 +19,27 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
-          {result.snippet}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
