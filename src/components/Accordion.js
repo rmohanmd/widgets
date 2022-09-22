567200bedd3b89a568fb9e9cd3519d8d6d454af3
rmohanmd <rmohan.md@gmail.com>
2022-09-22 15:14:27 -0400

    Added onClick eventHandler to detect click of titles in accordion app

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className="title active"
+          onClick={() => console.log("Title Clicked", index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
