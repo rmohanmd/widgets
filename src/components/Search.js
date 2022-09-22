f108983aea47f29e4f8a9541c28dadfc71b1a783
rmohanmd <rmohan.md@gmail.com>
2022-09-22 18:14:15 -0400

    Added debounce method
    Chagned the way the search was initiated by conducting search once debouncedTerm has updated.
    
    debounceTerm is updated only once the user has finished inputing a search term and no changes have been made
    for 500ms. Then useEffect will update debounceTerm and that will initiate the second useEffect that is monitoring
    debouncedTerm.

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,17 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("React");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 5000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,26 +23,14 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 500);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
