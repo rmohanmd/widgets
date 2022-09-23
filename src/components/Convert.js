197046e8c01c7e0340128150241f95ab1110d829
rmohanmd <rmohan.md@gmail.com>
2022-09-23 14:14:04 -0400

    Added debounce useEffect to reduce number of api calls until user has not input anythign for atleast 500ms

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,23 +2,44 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
-  const apiKey = `${process.env.REACT_APP_GOOGLE_TRANSLATE_KEY}`;
+  const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
 
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: apiKey,
-        },
-      }
-    );
-  }, [language, text]);
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
 
-  return <div />;
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const apiKey = `${process.env.REACT_APP_GOOGLE_TRANSLATE_KEY}`;
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: apiKey,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
