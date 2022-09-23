2d67078ccc82c2cd7854aa2aac095366e2e5ff29
rmohanmd <rmohan.md@gmail.com>
2022-09-23 00:32:35 -0400

    Added options array as props to Dropdown component

--- src/App.js
+++ src/App.js
@@ -18,12 +18,27 @@ const items = [
     content: "you use react by creating components",
   },
 ];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 export default () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown />
+      <Dropdown options={options} />
     </div>
   );
 };
