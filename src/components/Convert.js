8183e63add55aaebb0c6553e1ac38ef26bc41279
rmohanmd <rmohan.md@gmail.com>
2022-09-23 13:49:59 -0400

    Add google api request and implement api key

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const apiKey = `${process.env.REACT_APP_GOOGLE_TRANSLATE_KEY}`;
+
   useEffect(() => {
-    console.log("New language or text");
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: apiKey,
+        },
+      }
+    );
   }, [language, text]);
 
   return <div />;
