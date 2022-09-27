5e7f025d0bb72cf3608244b972cf21725cc141ca
rmohanmd <rmohan.md@gmail.com>
2022-09-27 13:00:41 -0400

    syntax changes

--- src/components/Route.js
+++ src/components/Route.js
@@ -4,6 +4,7 @@ const Route = ({ path, children }) => {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
   useEffect(() => {
+    
     const onLocationChange = () => {
       setCurrentPath(window.location.pathname);
     };
